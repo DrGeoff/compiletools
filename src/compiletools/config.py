bc402f48d1454fce505ae23f41012942f7cd0771
DrGeoff <drgeoffathome@gmail.com>
2025-10-08 17:32:43 -0500

    Fix bug in ct-config

--- src/compiletools/config.py
+++ src/compiletools/config.py
@@ -7,7 +7,7 @@ import compiletools.configutils
 
 
 def main(argv=None):
-    cap = compiletools.apptools.create_parser("Configuration examination tool", argv=argv, include_config=False)
+    cap = compiletools.apptools.create_parser("Configuration examination tool", argv=argv, include_config=True)
     compiletools.cake.Cake.add_arguments(cap)
     if argv is None:
         # Output of stdout is done via increasing the verbosity
