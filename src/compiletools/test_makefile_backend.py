d51dd64ea7097c20d2ccc2a81593618444ac80c9
Geoff Ericksson <gericksson@jumptrading.com>
2026-04-07 05:21:31 -0500

    Revert "perf: omit header prerequisites from Make compile rules"
    This reverts commit 386132a0b4f76ace8a6fc7ac5dfb2579715592a7.

--- src/compiletools/test_makefile_backend.py
+++ src/compiletools/test_makefile_backend.py
@@ -85,10 +85,7 @@ class TestMakefileGenerate:
         content = buf.getvalue()
 
         assert ".DELETE_ON_ERROR:" in content
-        # Compile rules omit normal prerequisites (content-addressed naming
-        # makes timestamp checking redundant); only order-only deps remain.
-        assert "obj/foo.o:" in content
-        assert "foo.cpp foo.h" not in content
+        assert "obj/foo.o: foo.cpp foo.h" in content
         assert "| /tmp/obj" in content
         assert "g++ -c foo.cpp -o obj/foo.o" in content
         assert "bin/foo: obj/foo.o" in content
